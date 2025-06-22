f1a5d8c665c7b199a2b050982d2e14b59ee69fe5
ConverseSea <dinoabraham@hotmail.com>
2025-06-22 12:07:25 -0600

    EndGame Trigger

---
+++ Assets/Scripts/Player/PlayerEndGame.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameTrigger : MonoBehaviour
+{
+    public GameObject puerta;             // Asigna aquí la puerta en el editor
+    public GameObject mensajeFinalUI;     // UI con "Gracias por jugar"
+
+    private bool gPressed = false;
+    private bool hPressed = false;
+    private bool triggered = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G)) gPressed = true;
+        if (Input.GetKeyUp(KeyCode.G)) gPressed = false;
+
+        if (Input.GetKeyDown(KeyCode.H)) hPressed = true;
+        if (Input.GetKeyUp(KeyCode.H)) hPressed = false;
+
+        if (gPressed && hPressed && !triggered)
+        {
+            triggered = true;
+            ActivarFinalDelJuego();
+        }
+    }
+
+    void ActivarFinalDelJuego()
+    {
+        if (puerta != null)
+        {
+            puerta.SetActive(false);  // "Abre" la puerta desactivándola
+        }
+
+        if (mensajeFinalUI != null)
+        {
+            mensajeFinalUI.SetActive(true);  // Muestra el mensaje de "Gracias por jugar"
+        }
+
+        Time.timeScale = 0; // Pausa el juego
+    }
+}
